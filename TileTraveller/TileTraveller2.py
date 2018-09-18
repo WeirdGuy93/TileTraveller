9bab1d6a9ccbd2577ffc0ce7033e88f9dc9ba8a0
WeirdGuy93 <bergura@hotmail.com>
2018-09-18 13:51:22 +0000

    commented the code (and a mior string tweak)

--- TileTraveller/TileTraveller2.py
+++ TileTraveller/TileTraveller2.py
@@ -4,21 +4,26 @@ import TileTravellerFunctions as functions
 x = 1
 y = 1
 
-
+#Set a bool to stop the game if won
 game_won = False
 
 while not game_won :
-
+    #Print out the available directions
     print(functions.get_available_directioins(x,y))
+    
+    #Get the direction the player wants to go
     player_move = input("Direction: ")
 
     new_position = functions.move_the_player_if_valid(player_move, x, y)
 
     if not new_position :
-        print("Not a valid position")
+        print("Not a valid direction!")
+    #Update coordinates if the direction is valid
     else :
         x = new_position[0]
         y = new_position[1]
+    
     game_won = functions.check_if_game_is_won(x,y)
 
+#Yay!
 print("Victory!")
